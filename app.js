d0937e33b7c1f4cc60af3e588c1aac08444305f2
“mateen1222” <mateenkaziawebdev@gmail.com>
2017-11-16 16:24:19 -0600

    Version @2.0.0

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mustache = require('mustache-express');
-var pg = require('pg')
 
 
 app.use(express.static('public'));
@@ -12,11 +11,10 @@ app.set('view engine', 'mustache');
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-
 app.use(session({
-  secret: 'KeyBoardCat',
-  resave: false,
-  saveUninitialized: true
+secret: 'KeyBoardCat',
+resave: false,
+saveUninitialized: true
 }))
 
 
@@ -32,21 +30,7 @@ app.use(likes);
 app.use(login);
 app.use(register);
 
-// Allows app to connect to Heroku database
-pg.defaults.ssl = true
-pg.connect(process.env.DATABASE_URL, function (err, client) {
-  if (err) throw err
-  console.log('Connected to postgres! Getting schemas...')
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function (row) {
-      console.log(JSON.stringify(row))
-    })
-})
 
 
-const port = process.env.PORT || 3000
-app.listen(port, function () {
-  console.log('Server is ON! Go to host:port:' + port)
-})
+app.listen(3000);
+console.log('Up and Running');
